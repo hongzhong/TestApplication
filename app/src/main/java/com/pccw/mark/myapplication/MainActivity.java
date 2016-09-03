22e4d92ba28098d8a494e9d1df7af270a338fdf9
Mark <781522317@qq.com>
2016-09-03 12:15:00 +0800

    第一次测试提交

--- app/src/main/java/com/pccw/mark/myapplication/MainActivity.java
+++ app/src/main/java/com/pccw/mark/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.pccw.mark.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        doGet();
 
+    }
+
+    private void doGet() {
+        Toast.makeText(this,"吐司",Toast.LENGTH_LONG).show();
     }
 }
