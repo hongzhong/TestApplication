0be62154386d0adcb0d7bafc441b72beb718554f
Mark <781522317@qq.com>
2016-09-03 12:35:48 +0800

    Mark第二次提交  添加Button

--- app/src/main/java/com/pccw/mark/myapplication/MainActivity.java
+++ app/src/main/java/com/pccw/mark/myapplication/MainActivity.java
@@ -2,15 +2,26 @@ package com.pccw.mark.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        doGet();
+        bt=(Button) findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doGet();
+            }
+        });
+
+
 
     }
 
